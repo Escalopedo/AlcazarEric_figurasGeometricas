c22841e2e5321d97b2270228c0d95594a0d1d85a
PC-ERIC <eric.alcazarc@gmail.com>
2024-10-24 17:53:02 +0200

    Todo corregido y verificado que funcione

--- introducir_lados.php
+++ introducir_lados.php
@@ -25,40 +25,40 @@ $figura = $_SESSION['figura'];
         
         <!-- Caso para Cuadrado -->
         <?php if ($figura == 'cuadrado') { ?>
-            <label for="lado1">Introduce el lado del cuadrado:</label>
+            <label for="lado1">Introduce el lado del cuadrado:</label><br><br>
             <input type="number" name="lado1" id="lado1" onblur="validarCampo('lado1')">
             <div id="error-lado1" class="error-message"></div> 
         <?php } ?>
         
         <!-- Caso para Círculo -->
         <?php if ($figura == 'circulo') { ?>
-            <label for="lado1">Introduce el radio del círculo:</label>
+            <label for="lado1">Introduce el radio del círculo:</label><br><br>
             <input type="number" name="lado1" id="lado1" onblur="validarCampo('lado1')">
             <div id="error-lado1" class="error-message"></div> 
         <?php } ?>
 
         <!-- Caso para Rectángulo -->
         <?php if ($figura == 'rectangulo') { ?>
-            <label for="lado1">Introduce el lado 1 del rectángulo:</label>
+            <label for="lado1">Introduce el lado 1 del rectángulo:</label><br><br>
             <input type="number" name="lado1" id="lado1" onblur="validarCampo('lado1')">
             <div id="error-lado1" class="error-message"></div> 
             <br><br>
-            <label for="lado2">Introduce el lado 2 del rectángulo:</label>
+            <label for="lado2">Introduce el lado 2 del rectángulo:</label><br><br>
             <input type="number" name="lado2" id="lado2" onblur="validarCampo('lado2')">
             <div id="error-lado2" class="error-message"></div> 
         <?php } ?>
 
         <!-- Caso para Triángulo -->
         <?php if ($figura == 'triangulo') { ?>
-            <label for="lado1">Introduce el lado 1 del triángulo:</label>
+            <label for="lado1">Introduce el lado 1 del triángulo:</label><br><br>
             <input type="number" name="lado1" id="lado1" onblur="validarCampo('lado1')">
             <div id="error-lado1" class="error-message"></div> 
             <br><br>
-            <label for="lado2">Introduce el lado 2 del triángulo:</label>
+            <label for="lado2">Introduce el lado 2 del triángulo:</label><br><br>
             <input type="number" name="lado2" id="lado2" onblur="validarCampo('lado2')">
             <div id="error-lado2" class="error-message"></div> 
             <br><br>
-            <label for="lado3">Introduce el lado 3 del triángulo:</label>
+            <label for="lado3">Introduce el lado 3 del triángulo:</label><br><br>
             <input type="number" name="lado3" id="lado3" onblur="validarCampo('lado3')">
             <div id="error-lado3" class="error-message"></div> 
         <?php } ?>
